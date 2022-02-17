8b630ecb90e397df6b99761c50046887c61e6a15
osas2211 <osaretin.frank10@gmail.com>
2022-02-17 05:49:08 +0100

    countdown timer fixed

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 export const Countdown = ()=>{
     //const [day_, setDay] = useState(2)
     const [hour_, setHour] = useState(48)
-    const [minute_, setMinute] = useState("00")
-    const [second_, setSecond] = useState("00")
+    const [minute_, setMinute] = useState("0")
+    const [second_, setSecond] = useState("0")
 
 
     const workingTimer = ()=>{
-        const countDate = new Date("February 16, 2022 00:00:00").getTime();
+        const countDate = new Date("February 19, 2022 00:00:00").getTime();
         const now = new Date().getTime();
         const gap = countDate - now;
         const second = 1000;
@@ -28,17 +28,17 @@ export const Countdown = ()=>{
             <h4 className="center mt-5">Ticket sale ends in: </h4>
             <div className="countdown-timer">
                 <div className="hr">
-                    <h3>{hour_}</h3>
+                    <h3>{(hour_ < 10) ? "0" + hour_: hour_}</h3>
                     <small>Hr</small>
                 </div>
                 <p>:</p>
                 <div className="min">
-                    <h3>{minute_}</h3>
+                    <h3>{(minute_ < 10) ? "0" + minute_: minute_}</h3>
                     <small>Min</small>
                 </div>
                 <p>:</p>
                 <div className="sec">
-                    <h3>{second_}</h3>
+                    <h3>{(second_ < 10) ? "0" + second_: second_}</h3>
                     <small>Sec</small>
                 </div>
             </div>
