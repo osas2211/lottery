0fd3e1e93394968456e32766ef68c98dec892f28
osas2211 <osaretin.frank10@gmail.com>
2022-03-13 17:13:28 +0100

    Added a DateValue.js File to this api folder

---
+++ src/api/DateValue.js
@@ -0,0 +1,8 @@
+// "ticketSaleStarts" is used in the CountDownEnding Component that is rendered once the ticket form is disabled
+
+// "ticketSaleEnds" is used in the TimerContext. This TimerContext manages the Ticket form, by disabling it onces the 48hrs is elapsed.
+
+export const dateValue = {
+    ticketSaleEnds: "March 15, 2022 00:00:00",
+    ticketSaleStarts: "March 17, 2022 00:00:00"
+}
