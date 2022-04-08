d11cdb6cd96b1e6f4ba1aa5de45255ba31fec42b
osas2211 <osaretin.frank10@gmail.com>
2022-04-08 23:11:37 +0100

    added winning category-1

--- src/components/RoundTickets.jsx
+++ src/components/RoundTickets.jsx
@@ -9,7 +9,7 @@ export default function RoundTickets({ setTicketModal}) {
         <div className='round-tickets'>
             <div className="prizepot-header d-flex"><h3>Round 495</h3> <span onClick={handleTicketModal}>X</span></div>
             <div className="content">
-                <p>Your Ticket</p>
+                <p>Your Tickets</p>
                 <small>#3</small>
                 <p className='num'>481117</p>
                 <p className='num'>481117</p>
